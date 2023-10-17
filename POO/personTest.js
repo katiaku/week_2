a457ab240f23cf4641c6ea869e48d88c9ced6d03
Katia <kushnir.ekaterina@gmail.com>
2023-10-17 15:05:16 +0200

    Correccion logs personTest

--- POO/personTest.js
+++ POO/personTest.js
@@ -4,7 +4,7 @@ let person1 = new Person("John", "Doe", 80, 1.72, ["comer", "dormir"]);
 console.log(person1);
 console.log(person1.calculateIMC());
 console.log(person1.edad(1985));
-console.log(person1.printAll());
-console.log(person1.printHobbies());
+person1.printAll();
+person1.printHobbies();
 
 module.exports = { person1 }
