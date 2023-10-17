04519a2d51cba0777776f64b1f4f0877dc3817e3
Katia <kushnir.ekaterina@gmail.com>
2023-10-17 14:00:03 +0200

    Clase Persona y su test

---
+++ POO/person.js
@@ -0,0 +1,37 @@
+class Person {
+    constructor(firstname, lastname, weight, height, hobbies = []) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.weight = weight;
+        this.height = height;
+        this.yearOfBirth;
+        this.hobbies = hobbies;
+    }
+
+    calculateIMC() {
+        let imc = this.weight / (this.height * this.height);
+        return imc
+    }
+
+    edad(yearOfBirth) {
+        let yearNow = new Date().getFullYear();
+        let age = yearNow - yearOfBirth;
+        return age
+    }
+
+    printAll() {
+        console.log("firstname - " + this.firstname);
+        console.log("lastname - " + this.lastname);
+        console.log("weight - " + this.weight);
+        console.log("height - " + this.height);
+        console.log("yearOfBirth - " + this.yearOfBirth);
+    }
+
+    printHobbies() {
+        for (let hobby in this.hobbies) {
+            console.log(this.hobbies[hobby]);
+        }
+    }
+}
+
+module.exports = { Person }
---
+++ POO/personTest.js
@@ -0,0 +1,8 @@
+let { Person } = require ("./person");
+
+let person1 = new Person("John", "Doe", 80, 1.72, ["comer", "dormir"]);
+console.log(person1);
+console.log(person1.calculateIMC());
+console.log(person1.edad(1985));
+console.log(person1.printAll());
+console.log(person1.printHobbies());
