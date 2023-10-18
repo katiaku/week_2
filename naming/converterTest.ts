245b38bf92191d4bf2b8c4ce11516272e6ef85a8
Katia <kushnir.ekaterina@gmail.com>
2023-10-18 21:23:10 +0200

    Retos opcionales 1-3

---
+++ naming/converterTest.ts
@@ -0,0 +1,6 @@
+// import { Converter } from ".";
+
+// let converter1 = new Converter("testing cases");
+// console.log(converter1.toCamel);
+// console.log(converter1.toPascal);
+// console.log(converter1.toSnake);
---
+++ naming/index.ts
@@ -0,0 +1,23 @@
+// import * as changeKeys from "change-case/keys";
+
+// export class Converter {
+
+//     public sentence: string;
+
+//     constructor(sentence: string) {
+//         this.sentence = sentence;
+//     }
+
+//     public toCamel(): string {
+//         return changeKeys.camelCase(this.sentence)
+//     }
+
+//     public toPascal(): string {
+//         return changeKeys.pascalCase(this.sentence)
+//     }
+
+//     public toSnake(): string {
+//         return changeKeys.snakeCase(this.sentence)
+//     }
+
+// }
